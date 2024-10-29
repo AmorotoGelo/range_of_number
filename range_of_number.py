a7d1efb9041853af74f583a46c19ae7bf54396f6
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2024-10-29 17:01:41 +0800

    Loop to ask user for number between 1-50, break if not an integer

--- range_of_number.py
+++ range_of_number.py
@@ -5,13 +5,16 @@ range_21_30 = 0
 range_31_40 = 0
 range_41_50 = 0
 # Loop to ask user for an input between 1 to 50
-
-# Ask user for input
-
+while True:
+    try:     
+        # Ask user for input
+        number = int(input("Please input a number ranging 1-50: "))  
 # Check if the input is out of range, error message and break the loop if out of range
 
 # Set the condition for the range and update quantity of the inputted numbers in range
 
 # Error message and break the loop if the input is not integer
-
+    except ValueError:
+        print("Invalid input")
+        break
 # Display the quantity of inputted numbers per range
