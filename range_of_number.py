9f2d8e6ab702a27b4243ed4b584e7cd7e63e1967
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2024-10-29 16:33:19 +0800

    Pseudocode

---
+++ range_of_number.py
@@ -0,0 +1,13 @@
+# Initialize the quantity per range
+
+# Loop to ask user for an input between 1 to 50
+
+# Ask user for input
+
+# Check if the input is out of range, error message and break the loop if out of range
+
+# Set the condition for the range and update quantity of the inputted numbers in range
+
+# Error message and break the loop if the input is not integer
+
+# Display the quantity of inputted numbers per range
